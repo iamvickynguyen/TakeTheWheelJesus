03897382884eec3860839998a48a003115327584
Vicky Nguyen <vicky.trucviennguyen@gmail.com>
2023-03-05 22:49:34 -0800

    clang-format

--- main.cpp
+++ main.cpp
@@ -1,40 +1,42 @@
-//HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
-//JSON credits: https://github.com/nlohmann/json
-#include <iostream>
-#include "./json.hpp"
+// HTTP credits: http://lcs.ios.ac.cn/~maxt/SPelton/reports/report-9aa0d3.html
+// JSON credits: https://github.com/nlohmann/json
 #include "./http_stuff.h"
+#include "./json.hpp"
 #include "./move.hpp"
+#include <iostream>
 using namespace std;
 using namespace nlohmann;
 int main(void) {
   httplib::Server svr;
   svr.Get("/", [](const auto &, auto &res) {
-    string head = "default"; //TODO: Change head
-    string tail = "default"; //TODO: Change tail
-    string author = ""; //TODO: Change your battlesnake username
-    string color = "#888888";  //TODO: Change a hex color
-    res.set_content("{\"apiversion\":\"1\", \"head\":\"" + head + "\", \"tail\":\"" + tail + "\", \"color\":\"" + color + "\", " + "\"author\":\"" + author + "\"}", "application/json");
+    string head = "default";  // TODO: Change head
+    string tail = "default";  // TODO: Change tail
+    string author = "";       // TODO: Change your battlesnake username
+    string color = "#888888"; // TODO: Change a hex color
+    res.set_content("{\"apiversion\":\"1\", \"head\":\"" + head +
+                        "\", \"tail\":\"" + tail + "\", \"color\":\"" + color +
+                        "\", " + "\"author\":\"" + author + "\"}",
+                    "application/json");
   });
-  svr.Post("/end", [](const auto &, auto &res){
+  svr.Post("/end", [](const auto &, auto &res) {
     res.set_content("ok", "text/plain");
   });
-  svr.Post("/start", [](const auto &, auto &res){
+  svr.Post("/start", [](const auto &, auto &res) {
     res.set_content("ok", "text/plain");
   });
-  svr.Post("/move", [](auto &req, auto &res){
+  svr.Post("/move", [](auto &req, auto &res) {
     const json data = json::parse(req.body);
     cout << data;
     cout << "\n\n";
-    //You can get the "you" property like this:
-    //data["you"];
-    //Almost alike python dictionary parsing, but with a semicolon at the end of each line.
-    //You might need to make some structs to store some data in a variable
-    //Example:
-    //you_struct you = data["you"];
+    // You can get the "you" property like this:
+    // data["you"];
+    // Almost alike python dictionary parsing, but with a semicolon at the end
+    // of each line. You might need to make some structs to store some data in a
+    // variable Example: you_struct you = data["you"];
     string moves[4] = {"up", "down", "left", "right"};
-    //int index = rand() % 4;
+    // int index = rand() % 4;
 
-		int index = cppssss::move(data);
+    int index = cppssss::move(data);
 
     res.set_content("{\"move\": \"" + moves[index] + "\"}", "text/plain");
   });
--- move.hpp
+++ move.hpp
@@ -5,9 +5,7 @@
 
 // 0:up, 1:down, 2:left, 3:right
 namespace cppssss {
-	int move(const nlohmann::json data) {
-		return 1;
-	}
-}
+int move(const nlohmann::json data) { return 1; }
+} // namespace cppssss
 
-#endif  // INCLUDE_MOVE_HPP
+#endif // INCLUDE_MOVE_HPP
