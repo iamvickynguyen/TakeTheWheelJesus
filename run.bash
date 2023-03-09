e18a9197fe2624b7a628814364ea306d7c1c24ef
Grey Hill <infogreytech@gmail.com>
2023-03-09 06:17:47 -0800

    Add echos
    The std::cout in the main function isn't being written to my console for
    some reason. These echos at least tell me something is running.

--- run.bash
+++ run.bash
@@ -1,3 +1,8 @@
 echo This can take some time, there are over 30000 lines of code to compile in the libraries...
 clang++ -pthread -std=c++17 -o main main.cpp
+
+echo "Build complete"
+
+echo "Running snake..."
 ./main
+
